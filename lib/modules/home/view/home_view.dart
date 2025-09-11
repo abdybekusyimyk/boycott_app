848f0e84dcf889e60686f631d31013eb91b2e5fe
Userabubakr2008 <ishuchun1122@gmail.com>
2025-09-11 19:12:27 +0600

    home  tugirlandi

--- lib/modules/home/view/home_view.dart
+++ lib/modules/home/view/home_view.dart
@@ -33,52 +33,42 @@ class HomeView extends StatelessWidget {
                 itemCount: 10,
                 gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                   crossAxisCount: 2,
-                  crossAxisSpacing: 17,
-                  mainAxisSpacing: 17,
+                  crossAxisSpacing: 8,
+                  mainAxisSpacing: 8,
                 ),
                 itemBuilder: (context, index) {
                   return Container(
                     decoration: BoxDecoration(
                       color: AppColor.grengrey,
-                      borderRadius: BorderRadius.circular(20),
-                      // shape: BoxShape.circle,
-                      boxShadow: [
-                        BoxShadow(
-                          offset: Offset(-1.9, 1),
-                          color: const Color.fromARGB(214, 174, 169, 166),
-                        ),
-                      ],
-                      // border: Border.symmetric(
-                      //   horizontal: BorderSide(
-                      //     color: AppColor.black,
-                      //     width: 0.4,
-                      //   ),
-                      //   vertical: BorderSide(
-                      //     color: AppColor.black,
-                      //     width: 0.45,
-                      //   ),
-                      // ),
+                      borderRadius: BorderRadius.circular(12),
                     ),
-                    child: Column(
+                    child: Stack(
                       children: [
-                        Align(
-                          alignment: Alignment.topLeft,
-                          child: Transform(
-                            alignment: Alignment.topRight,
-                            transform: Matrix4.identity()..rotateZ(-0.6),
+                        Positioned(
+                          top: 5,
+                          left: 5,
+                          child: Transform.rotate(
+                            angle: -0.5, // radian (taxminan -23 gradus)
                             child: Text(
                               'Boycott',
                               style: AppTypography.red14w300,
                             ),
                           ),
                         ),
-                        SizedBox(height: 35),
-                        Image.asset("assets/images/baycott.png", height: 110),
-                        SizedBox(height: 10),
-                        Text(
-                          "https:api.boycottisraeli.bi...",
-                          style: AppTypography.black12w300,
-                          maxLines: 1,
+                        Align(
+                          alignment: Alignment.center,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Image.asset("assets/images/baycott.png"),
+                              SizedBox(height: 15),
+                              Text(
+                                "https:api.boycottisraeli.bi...",
+                                style: AppTypography.black12w300,
+                                maxLines: 1,
+                              ),
+                            ],
+                          ),
                         ),
                       ],
                     ),
