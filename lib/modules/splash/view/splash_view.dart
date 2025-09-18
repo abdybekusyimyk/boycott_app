f69d4252e68325223824ddac57971711571f50e5
Abdybek uulu Syimyk <abdybekuulusyimyk@gmail.com>
2025-09-18 22:04:05 +0600

    SplashView finshed

--- lib/modules/splash/view/splash_view.dart
+++ lib/modules/splash/view/splash_view.dart
@@ -1,4 +1,5 @@
 import 'package:boycott_app/modules/onboarding/onboarding_controller/controller.dart';
+import 'package:boycott_app/theme/colors/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class SplashView extends StatefulWidget {
@@ -8,51 +9,50 @@ class SplashView extends StatefulWidget {
   State<SplashView> createState() => _SplashViewState();
 }
 
-class _SplashViewState extends State<SplashView>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _animation;
+class _SplashViewState extends State<SplashView> with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+  late final Animation<double> _animation;
+
+  static const _splashDuration = Duration(seconds: 3);
+  static const _navigateDelay = Duration(seconds: 2);
 
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(
-      vsync: this,
-      duration: Duration(seconds: 3),
-    );
+    _controller = AnimationController(vsync: this, duration: _splashDuration);
     _animation = Tween<double>(
       begin: 0.7,
       end: 3,
     ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
     _controller.forward();
-    Future.delayed(Duration(seconds: 2), () {
-      Navigator.of(
-        // ignore: use_build_context_synchronously
-        context,
-      ).pushReplacement(MaterialPageRoute(builder: (_) => OnboardingScreen()));
+
+    Future.delayed(_navigateDelay, () {
+      if (mounted) {
+        Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (ctx) => const OnboardingScreen()));
+      }
     });
   }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    backgroundColor: AppColor.white,
+    body: Center(
+      child: ScaleTransition(
+        scale: _animation,
+        child: SizedBox.square(
+          dimension: 80,
+          child: Image.network(
+            "https://www.shutterstock.com/shutterstock/photos/2579138043/display_1500/stock-vector-boycott-on-black-splash-vector-2579138043.jpg",
+            fit: BoxFit.contain,
+          ),
+        ),
+      ),
+    ),
+  );
 
   @override
   void dispose() {
     _controller.dispose();
     super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ScaleTransition(
-          scale: _animation,
-          child: Image.network(
-            "https://www.shutterstock.com/shutterstock/photos/2579138043/display_1500/stock-vector-boycott-on-black-splash-vector-2579138043.jpg",
-            width: 80, // kerakli kenglik
-            height: 90, // kerakli balandlik
-            fit: BoxFit.contain,
-          ),
-        ),
-      ),
-    );
   }
 }
