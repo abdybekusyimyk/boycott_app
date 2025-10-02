0509d9ebf4326d03cd71ce7558fe88e91c4161fe
Userabubakr2008 <ishuchun1122@gmail.com>
2025-10-02 22:36:17 +0600

    It has been added: About Us, Feedback, and Language

---
+++ lib/modules/feedback/view/feedback_view.dart
@@ -0,0 +1,80 @@
+import 'package:boycott_app/theme/colors/app_colors.dart';
+import 'package:boycott_app/theme/typography/app_typography.dart';
+import 'package:flutter/material.dart';
+
+class FeedbackView extends StatelessWidget {
+  const FeedbackView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColor.white,
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios, color: AppColor.darkGray, size: 24),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        backgroundColor: AppColor.white,
+        elevation: 0,
+        title: Text('Feedback', style: AppTypography.darkGray22w600),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Center(
+              child: Text(
+                "We value your opinion",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                  color: AppColor.darkGray, // Matn rangini yaxshiladik
+                ),
+              ),
+            ),
+            const SizedBox(height: 12),
+            Stack(
+              alignment: Alignment.bottomRight,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    color: Color(
+                      0xFFD3D3D3,
+                    ), // TextField fon rangi, ko‘zga qulay
+                  ),
+                  child: TextField(
+                    maxLines: 4,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Enter your feedback here',
+                      hintStyle: TextStyle(
+                        color: AppColor.darkGray,
+                        fontSize: 16,
+                      ),
+                      contentPadding: const EdgeInsets.all(16.0),
+                    ),
+                    style: TextStyle(color: AppColor.darkGray, fontSize: 16),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 15),
+            Align(
+              alignment: Alignment.centerRight,
+              child: TextButton(
+                onPressed: () {
+                  print("Feedback sent");
+                },
+                child: Text("Send", style: AppTypography.darkGray22w600),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/modules/profile/view/profile_view.dart
+++ lib/modules/profile/view/profile_view.dart
@@ -1,10 +1,19 @@
-import 'package:boycott_app/modules/profile/widgets/button_profile.dart';
+import 'package:flutter/material.dart';
 import 'package:boycott_app/theme/colors/app_colors.dart';
 import 'package:boycott_app/theme/typography/app_typography.dart';
-import 'package:flutter/material.dart';
+import 'package:boycott_app/modules/about_us/view/about_us_view.dart';
+import 'package:boycott_app/modules/feedback/view/feedback_view.dart';
+import 'package:boycott_app/modules/profile/widgets/button_profile.dart';
 
-class ProfileView extends StatelessWidget {
+class ProfileView extends StatefulWidget {
   const ProfileView({super.key});
+
+  @override
+  State<ProfileView> createState() => _ProfileViewState();
+}
+
+class _ProfileViewState extends State<ProfileView> {
+  String selectedLanguage = "English";
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +21,9 @@ class ProfileView extends StatelessWidget {
       backgroundColor: AppColor.white,
       appBar: AppBar(
         backgroundColor: AppColor.white,
-        title: Text("Profile", style: AppTypography.darkGray22w600),
+        title: Center(
+          child: Text("Profile", style: AppTypography.darkGray22w600),
+        ),
       ),
       body: Column(
         children: [
@@ -20,14 +31,96 @@ class ProfileView extends StatelessWidget {
           ButtonProfile(
             icon: Icons.info_outline,
             title: "About us",
-            onTap: () {},
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const AboutUsView()),
+              );
+            },
           ),
           SizedBox(height: 12),
           ButtonProfile(icon: Icons.share, title: "Share", onTap: () {}),
           SizedBox(height: 12),
-          ButtonProfile(icon: Icons.code, title: "For developer", onTap: () {}),
+          ButtonProfile(
+            icon: Icons.feedback,
+            title: "Feedback",
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const FeedbackView()),
+              );
+            },
+          ),
           SizedBox(height: 12),
-          ButtonProfile(icon: Icons.settings, title: "Settings", onTap: () {}),
+          ButtonProfile(
+            icon: Icons.language,
+            title: "Language",
+            trailingText: selectedLanguage,
+            onTap: () {
+              showModalBottomSheet(
+                backgroundColor: Colors.transparent,
+                context: context,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+                ),
+                builder: (context) {
+                  return Padding(
+                    padding: const EdgeInsets.all(24.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: AppColor.white,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Text(
+                              "Language",
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          // English
+                          RadioListTile<String>(
+                            title: Text("English"),
+                            value: "English",
+                            // ignore: deprecated_member_use
+                            groupValue: selectedLanguage,
+                            // ignore: deprecated_member_use
+                            onChanged: (value) {
+                              setState(() {
+                                selectedLanguage = value!;
+                              });
+                              Navigator.pop(context);
+                            },
+                          ),
+                          // Kyrgyz
+                          RadioListTile<String>(
+                            title: Text("Kyrgyz"),
+                            value: "Kyrgyz",
+                            // ignore: deprecated_member_use
+                            groupValue: selectedLanguage,
+                            // ignore: deprecated_member_use
+                            onChanged: (value) {
+                              setState(() {
+                                selectedLanguage = value!;
+                              });
+                              Navigator.pop(context);
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              );
+            },
+          ),
         ],
       ),
     );
