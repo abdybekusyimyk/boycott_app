0ee2c49cb52b7044dc9fb99be46d3f1f1bfb7d90
Abdybek uulu Syimyk <abdybekuulusyimyk@gmail.com>
2025-10-06 17:40:51 +0600

    datan-> home

---
+++ lib/data/home/models/home.dart
@@ -0,0 +1,9 @@
+// class HomeModel {
+//   final List<dynamic> companies;
+
+//   HomeModel({required this.companies});
+
+//   factory HomeModel.fromJson(Map<String, dynamic> json) {
+//     return HomeModel(companies: json['companies'] as List<dynamic>);
+//   }
+// }
---
+++ lib/data/home/repositories/home_repo_impl.dart
@@ -0,0 +1,17 @@
+// import 'package:boycott_app/data/home/repositories/home_repository.dart';
+// import 'package:boycott_app/data/home/sources/home_data_source.dart';
+// import 'package:boycott_app/data/home/models/home.dart';
+// import 'package:flutter/foundation.dart';
+
+// @immutable
+// final class HomeRepositoryImpl implements HomeRepository {
+//   const HomeRepositoryImpl({required this.dataSource});
+
+//   final HomeDataSource dataSource;
+
+//   @override
+//   Future<HomeModel> getHome() async {
+//     final data = await dataSource.getHome();
+//     return data;
+//   }
+// }
